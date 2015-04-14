8eab870ee6cbd0217ab784cf7a70ca053b05c270
jhelie <jean.helie@gmail.com>
2015-04-14 13:29:54 +0100

    v0.1.4

--- TM_density.py
+++ TM_density.py
@@ -12,7 +12,7 @@ import os.path
 #=========================================================================================
 # create parser
 #=========================================================================================
-version_nb = "0.1.3"
+version_nb = "0.1.4"
 parser = argparse.ArgumentParser(prog = 'TM_density', usage='', add_help = False, formatter_class = argparse.RawDescriptionHelpFormatter, description =\
 '''
 **********************************************
